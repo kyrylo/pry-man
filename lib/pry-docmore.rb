cdafdfd014c6620e247c3aac80d2a814cdff4082
☈king <rking@sharpsaw.org>
2012-11-19 06:51:04 +0000

    Actually decorates `show-doc` (and `?`) Yeah!
    Thanks @ConradIrwin for the pair session that pushed this one over the
    top.

--- lib/pry-docmore.rb
+++ lib/pry-docmore.rb
@@ -2,21 +2,8 @@
 require 'pry'
 class Pry
   class Docmore
-    Pry.commands.command '/?' do
-      Pry.output.puts Pry::Docmore.explain arg_string.strip
-      prev = Pry.commands.entries.find do |cmd, inst| cmd == 'show-doc' end[1]
-      p prev
-      blk = prev.instance_variable_get :@block
-      p blk
-      blk.call arg_string
-    end
-
-    Pry.commands.command ',?', 'Doc more — see global/keyword info' do
-      _pry_.output.puts "Note: The ,? command will be the plain ? command.\n" +
-        Pry::Docmore.explain(arg_string)
-    end
-
     def self.find_docs pattern
+      # XXX File.basename(__FILE__) +
       input = Dir['pry-docmore.wiki/*.md'].map do |file_name|
         if file_name.match pattern
           item = $1
@@ -26,23 +13,51 @@ class Pry
       end.compact
       Hash[input]
     end
-    @DOCS = [
-      {
-        label: 'Keyword',
+    @DOCS = {
+      'Keyword' => {
         source: 'ruby source, lex.c, circa line 219',
         explanations: find_docs(%r/Ruby Keyword: (.+)\.md/)
       },
-      {
-        label: 'Global Variable',
+      'Global Variable' => {
         source: 'Started from: http://www.zenspider.com/Languages/Ruby/QuickRef.html#pre-defined-variables',
         explanations: find_docs(%r/Ruby Global (\S+)/)
       },
-    ]
+    }
+    def self.raw; @DOCS end
     def self.explain thing
-      @DOCS.each do |kind|
-        return kind[:explanations][thing] if kind[:explanations].has_key? thing
+      @DOCS.each do |label, docs|
+        # TODO: colored label
+        return docs[:explanations][thing] if docs[:explanations].has_key? thing
       end
       nil
     end
+
+    ORIGINAL_SHOW_DOC ||= Pry.commands['show-doc']
+    Pry::Commands.create_command 'show-doc' do
+      description 'Show the documentation for a method/class/keyword/global. Aliases: ?'
+      command_options \
+        :shellwords => false,
+        :keep_retval => false
+      def process arg_string
+        if docmore = Pry::Docmore.explain(arg_string)
+          _pry_.output.puts docmore
+          $/
+        else
+          ORIGINAL_SHOW_DOC.new(context).call_safely(arg_string)
+        end
+      end
+    end
+    Pry::Commands.create_command 'show-docmores' do
+      description 'List keywords and vars covered by pry-docmore'
+      def process
+        Pry::Docmore.raw.each do |label, docs|
+          # Pry needs a general mechanism to columnize things, and when it gets
+          # one, this can use it:
+          _pry_.output.puts "#{label}s".yellow + ': ' +
+            docs[:explanations].keys.join(' ')
+        end
+      end
+    end
+
   end
 end
